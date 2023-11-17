da779ac027e6071b054a3512c6cef21c51b62cb1
DerogeNaN <132528861+DerogeNaN@users.noreply.github.com>
2023-11-17 14:42:10 +1100

    Implemented character customization phase.

--- Assets/Scripts/CharacterCustomizer.cs
+++ Assets/Scripts/CharacterCustomizer.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterCustomizer : MonoBehaviour
 {
-    GameObject[] hatArray = { };
+    [SerializeField] GameObject[] hatArray = new GameObject[5];
     PlayerController playerController;
     int curHatIndex = 0;
 
@@ -16,18 +17,37 @@ public class CharacterCustomizer : MonoBehaviour
     }
 
 
-    void Update()
-    {
-        
-    }
-
     public void IterateLeft(InputAction.CallbackContext callback)
     {
+        if (curHatIndex == 0)
+        {
+            hatArray[curHatIndex].gameObject.SetActive(false);
+            curHatIndex = 4;
+            hatArray[curHatIndex].gameObject.SetActive(true);
+        }
+        else
+        {
+            hatArray[curHatIndex].gameObject.SetActive(false);
+            curHatIndex--;
+            hatArray[curHatIndex].gameObject.SetActive(true);
+        }
         Debug.Log("IterateLeft");
     }
 
     public void IterateRight(InputAction.CallbackContext callback)
     {
+        if (curHatIndex == 4)
+        {
+            hatArray[curHatIndex].gameObject.SetActive(false);
+            curHatIndex = 0;
+            hatArray[curHatIndex].gameObject.SetActive(true);
+        }
+        else
+        {
+            hatArray[curHatIndex].gameObject.SetActive(false);
+            curHatIndex++;
+            hatArray[curHatIndex].gameObject.SetActive(true);
+        }
         Debug.Log("IterateRight");
     }
 }
